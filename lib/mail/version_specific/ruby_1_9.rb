acd9ff497ad81d76068c3f35e5a29e6d4d08081d
Michael Reinsch <michael@mobalean.com>
2014-09-28 01:13:03 +0900

    don't raise UndefinedConversionError
    * skip unconvertable characters as we do with invalid characters

--- lib/mail/version_specific/ruby_1_9.rb
+++ lib/mail/version_specific/ruby_1_9.rb
@@ -86,8 +86,8 @@ module Mail
         str = Ruby19.decode_base64(match[2])
         str = charset_encoder.encode(str, charset)
       end
-      decoded = str.encode(Encoding::UTF_8, :invalid => :replace, :replace => "")
-      decoded.valid_encoding? ? decoded : decoded.encode(Encoding::UTF_16LE, :invalid => :replace, :replace => "").encode(Encoding::UTF_8)
+      decoded = str.encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace, :replace => "")
+      decoded.valid_encoding? ? decoded : decoded.encode(Encoding::UTF_16LE, :invalid => :replace, :undef => :replace, :replace => "").encode(Encoding::UTF_8)
     rescue Encoding::UndefinedConversionError, ArgumentError, Encoding::ConverterNotFoundError
       warn "Encoding conversion failed #{$!}"
       orig_str.dup.force_encoding(Encoding::UTF_8)
